e47d0842bf82d385bd726d768667d75b6485fadd
ejshower <ejshower@gmail.com>
2024-05-22 20:41:27 +0900

    axios 오타로 오류발생 주의

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  let [cnt, setCnt] = useState(0);
-  let ThumbClick = () => {
-    setCnt(cnt + 1);
-  };
+  // let [cnt, setCnt] = useState(0);
+  // let ThumbClick = () => {
+  //   setCnt(cnt + 1);
+  // };
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/photos')
+         .then(response => setPosts(response.data))
+  },[]);
+
   return (
     <div>
-      <span onClick={ThumbClick} Style={"cursor:pointer"}>👍</span><span>{cnt}</span>      
+      {/* <span onClick={ThumbClick} Style={"cursor:pointer"}>👍</span><span>{cnt}</span>     */}
+
+      <ul>
+        {/*화살표 함수에서 {return} or ({})*/
+          posts.map(post => (
+            <li key={post.id}>
+                <div>{post.title}</div>
+                <div><img src={post.thumbnailUrl}/></div>
+            </li>
+          ))  
+        }
+      </ul>  
     </div>
   );
 }
